a9666e0ddddf132f22c71ee6c3f9fefd69391ff9
Quang.TranVan <quang.tranvan@powergatesoftware.com>
2021-11-09 08:45:05 +0700

    Create

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import './App.css';
-
+import faker from 'faker';
+import { useEffect, useState } from 'react';
+import { AutoSizer, List } from 'react-virtualized';
+import { height } from 'dom-helpers';
 function App() {
-  return <div>Test</div>;
+  const [people, setPeople] = useState([]);
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    setPeople(
+      [...Array(10000).keys()].map((key) => {
+        return {
+          id: key,
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          bio: faker.lorem.lines(Math.random() * 100),
+        };
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div>
+      <h1>{time.toISOString()}</h1>
+      <div>
+        <AutoSizer style={{ width: '100%', height: '100vh' }}>
+          {({ width, height }) => (
+            <List
+              width={width}
+              height={height}
+              rowHeight={50}
+              rowCount={people.length}
+              rowRenderer={({ key, index, style, parent }) => {
+                const person = people[index];
+                return (
+                  <div key={key} style={style}>
+                    <h2>{person.name}</h2>
+                  </div>
+                );
+              }}
+            />
+          )}
+        </AutoSizer>
+      </div>
+
+      {/* <ul>
+        {people.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
 }
 
 export default App;
